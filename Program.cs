cf20d5b4793f39dec04b143989df5898e868ed6d
klon03 <wachowski.kajetan@gmail.com>
2023-01-30 03:17:57 +0100

    tiding up

--- Program.cs
+++ Program.cs
@@ -11,19 +11,11 @@ namespace LibraryManager
         public static Catalog Catalog;
         static void Main(string[] args)
         {
-            //Dictionary<string, string> actions = new Dictionary<string, string>();
-
-            //Book lotr = new Book("Lord of the Rings;Akcja, kosmici, elfy; 15; 1", "Przygodowa", 1);
-            //lotr.ShowData();
-
             Catalog = new Catalog(@"data\books.csv");
-            //Catalog.ShowCategories();
-
 
             Frontend ui = new Frontend();
             while (true)
             {
-                
                 ui.ShowActions();
                 Console.Write("\nWybór: ");
                 ui.input = Console.ReadLine();
